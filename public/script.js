7f47764910622911158f66cbf1436ba144671f33
jhgirald <humberto.giraldez@gmail.com>
2023-12-07 22:00:45 -0700

    Added third party call to functions and recipes

--- public/script.js
+++ public/script.js
@@ -20,3 +20,58 @@ function getCookie(name) {
     return null;
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  username = getCookie("username");
+  if(username != null){
+  document.getElementById('welcome_text').innerText = "Welcome " + username + "!";
+  }
+  else {
+    window.location.href = `login.html`
+  }
+  showPopup();
+});
+function showPopup() {
+
+document.getElementById('welcomePopup').style.display = 'block';
+document.getElementById('overlay').style.display = 'block';
+}
+function closePopup() {
+document.getElementById('welcomePopup').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
+}
+function loadPopup(){
+  document.addEventListener('DOMContentLoaded', function() {
+    username = getCookie("username");
+    if(username != null){
+    document.getElementById('welcome_text').innerText = "Welcome " + username + "!";
+    }
+    else {
+      window.location.href = `login.html`
+    }
+    showPopup();
+  });
+}
+function get_general_recipes(){
+import("cookhub").then(async (cookhubModule) => {
+  const { CooklangRepository } = cookhubModule;
+
+const repo = new CooklangRepository();
+
+await repo.initialize("nicholaswilde", "recipes", "cook/breads");
+
+const recipes = repo.recipes;
+
+return recipes;
+});
+
+}
+
+
+function quote(){
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('welcome_quote').innerText = data.content;
+      document.getElementById('quote_author').innerText = data.author;
+    });
+}
