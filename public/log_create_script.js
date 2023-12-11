733d2bc6ff513f3b214d7e8533e55b6fb4b816d2
jhgirald <humberto.giraldez@gmail.com>
2023-12-10 23:29:37 -0700

    Added create account functionality

--- public/log_create_script.js
+++ public/log_create_script.js
@@ -12,6 +12,20 @@ function setCookie(name, val) {
     console.error('Error:', error);
   });
 }
+
+function createUser(username, password){
+  const url = `/user/create/${username.value}/${password.value}`;
+  fetch(url, {
+    method: 'POST',
+  })
+  .then((response) => response.json())
+  .then((jsonResponse) => {
+    console.log(jsonResponse);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
 async function logIn() {
     const username = document.getElementById("username");
     const password = document.getElementById("password");
@@ -30,7 +44,8 @@ function showLoginFailMessage() {
   }
 
 
-function createAccount(){
+
+async function createAccount(){
     const username = document.getElementById("username");
     const password = document.getElementById("password");
     const repassword = document.getElementById("repassword");
@@ -42,7 +57,32 @@ function createAccount(){
     usernameLabel.textContent = "Username";
 
     if (validatePassword(password) && validatePassword(repassword) && password.value === repassword.value){
+
+      const url = `/user/create/${username.value}/${password.value}`;
+      let jsonResponse;
+      fetch(url, {
+        method: 'POST',
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        jsonResponse = data;
+        console.log(jsonResponse);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+      valid = false;
+      await new Promise(r => setTimeout(r, 1000));
+      if(jsonResponse["error"] == "Username already taken"){
+        usernameLabel.style.color = "red";
+        usernameLabel.textContent = "Check username";
+      }
+      else{
+        
+        setCookie("username", username.value);
+        await new Promise(r => setTimeout(r, 1000));
         window.location.href = `main.html`;//get auth token, create account and info of the user and send it over
+      }
     }else{
         passwordLabel.style.color = "red";
         passwordLabel.textContent = "Check passwords";
